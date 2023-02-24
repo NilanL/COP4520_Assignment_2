c6c2eb88c6e4bfeea58633f567c7ae7de80e26f2
NilanL <54152496+NilanL@users.noreply.github.com>
2023-02-23 20:50:25 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,133 +1,11 @@
 import java.util.*;
-//import java.io.*;
 
 public class Main {
-    double execTime;
-    Thread [] threads;
-    int numberOfGuests;
-
-    int counter = 0;
-    volatile boolean hasCupcake = true;
-    volatile boolean allGuestsHaveGone = false;
-
-    Main()
+    public static void part1(Scanner scanner)
     {
-        execTime = 0;
-        numberOfGuests = 8;
-        threads = new Thread [8];
-    }
-
-    Main(int n)
-    {
-        execTime = 0;
-        numberOfGuests = n;
-        threads = new Thread [n];
-    }
-
-    public class MazeRunnable implements Runnable
-    {
-        static int N;
-        boolean isLeader;
-    
-        MazeRunnable(int n, boolean isLeader)
-        {
-            N = n;
-            this.isLeader = isLeader;
-        }
-    
-        @Override
-        public void run()
-        {
-            synchronized(this)
-            {
-                
-                System.out.println((isLeader ? "Leader" : "Guest ")  + " | threadID - " + Thread.currentThread().getId() + " | Counted people: " + counter + " | Has Cupcake: " + hasCupcake);
-                
-                // Labyrinth stuff happens here
-                // But for the sake of performance let's skip that part
-
-                if (isLeader)
-                {
-                    if (hasCupcake)
-                    {
-                        // Leave cupcake alone
-                    }
-                    else
-                    {
-                        // Ask for another cupcake
-                        hasCupcake = true;
-
-                        counter++;
-
-                        // If leader has accounted for all guests
-                        // other than himself/herself
-                        if (counter == N - 1)
-                        {
-                            allGuestsHaveGone = true;
-                        }
-                    }
-                }
-                else
-                {
-                    if (hasCupcake)
-                    {
-                        // Eat cupcake
-                        hasCupcake = false;
-                    }
-                    else
-                    {
-                        // Do not ask for another cupcake
-                    }
-                }
-            }
-        }
-    }
-    
-
-    // Begins the prime accounting process
-    public void runProblem1()
-    {
-        long startTime = System.nanoTime();
-        Random rand = new Random((long)Math.random());
-        MazeRunnable leaderRunnable = new MazeRunnable(numberOfGuests, true);
-        MazeRunnable guestRunnable = new MazeRunnable(numberOfGuests, false);
-
-        while (!allGuestsHaveGone)
-        {
-            // Randomly selected guest
-            int index = rand.nextInt(threads.length);
-            
-            // index 0 is the designated leader
-            if (index == 0)
-            {
-                threads[index] = new Thread(leaderRunnable);
-            }
-            else
-            {
-                threads[index] = new Thread(guestRunnable);
-            }
-
-            threads[index].start();
-
-            try
-            {
-                threads[index].join();
-            }
-            catch (InterruptedException e)
-            {
-                System.out.println(e.getMessage());
-                return;
-            }
-        }
-
-        execTime = (System.nanoTime() - startTime) / 1000000.0;
-    }
-
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
         int input = 8;
 
-        System.out.println("Part 1");
+        System.out.println("\nPart 1 (Cupcake problem)");
         System.out.println("-------------------------------------------------");
         System.out.println("How many guests?");
 
@@ -139,11 +17,16 @@ public class Main {
             {
                 getInput = false;
                 input = scanner.nextInt();
+
+                if (input < 0)
+                {
+                    throw new InputMismatchException();
+                }
             }
             catch (InputMismatchException e)
             {
                 System.out.println(e.getMessage());
-                System.out.println("Incorrect type, please input an integer number of guests");
+                System.out.println("Incorrect type, please input a positive integer number of guests");
                 scanner.next();
                 getInput = true;
             }
@@ -155,10 +38,65 @@ public class Main {
             }
         }
         
-        scanner.close();
+        // Run operation
+        Problem1 p1 = new Problem1(input);
+        p1.runProblem1();
 
-        Main assignment2 = new Main(input);
-        assignment2.runProblem1();
-        
+        System.out.println("-------------------------------------------------");
+        System.out.println("EXECUTION TIME: " + p1.getExecutionTime() + "ms\n");
+    }
+
+    public static void part2(Scanner scanner)
+    {
+        int input = 8;
+
+        System.out.println("\nPart 2 (Vase Problem)");
+        System.out.println("-------------------------------------------------");
+        System.out.println("How many guests?");
+
+        boolean getInput = true;
+
+        while (getInput)
+        {
+            try
+            {
+                getInput = false;
+                input = scanner.nextInt();
+
+                if (input < 0)
+                {
+                    throw new InputMismatchException();
+                }
+            }
+            catch (InputMismatchException e)
+            {
+                System.out.println(e.getMessage());
+                System.out.println("Incorrect type, please input a positive integer number of guests");
+                scanner.next();
+                getInput = true;
+            }
+            catch (Exception e)
+            {
+                System.out.println("ERROR: " + e.getMessage());
+                scanner.close();
+                return;
+            }
+        }
+
+        // Run operation
+        Problem2 p2 = new Problem2(input);
+        p2.runProblem2();
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("EXECUTION TIME: " + p2.getExecutionTime() + "ms\n");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        part1(scanner);
+        part2(scanner);
+
+        scanner.close();
     }
 }
